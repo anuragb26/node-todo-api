1bb9276a975cfae55fe6bf851a7b42fe256e1a70
Anurag Bajaj <AnuragB@talentica-all.com>
2018-12-05 00:14:46 +0530

    Chore:Get all todos with test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,7 +4,22 @@ const request = require('supertest')
 const { app } = require('../server')
 const { Todo } = require('../models/todo')
 
-beforeEach((done) => {Todo.remove({}).then(() => done()) })
+const todos = [
+  {
+    text: 'First test todo'
+  },
+  {
+    text: 'Second test todo'
+  }
+]
+beforeEach((done) =>
+{
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos)
+  }).then((data) => {
+    done()
+  })
+})
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
@@ -20,7 +35,7 @@ describe('POST /todos', () => {
         if (err) {
           return done(err)
         }
-        Todo.find().then(todos => {
+        Todo.find({ text }).then(todos => {
           expect(todos.length).toBe(1)
           expect(todos[0].text).toBe(text)
           done()
@@ -39,11 +54,27 @@ describe('POST /todos', () => {
           return done(err)
         }
         Todo.find().then(todos => {
-          expect(todos.length).toBe(0)
+          expect(todos.length).toBe(2)
           done()
         }).catch(err => {
           done(err)
         })
+      })
+  })
+})
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => expect(res.body.todos.length).toBe(2)) // will not get error callback
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        expect(res.body.todos.length).toBe(2)
+        done()
       })
   })
 })
