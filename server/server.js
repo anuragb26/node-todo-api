c0dbed0e0aeb9fe53c47fa37574ad880706f5b42
Anurag Bajaj <AnuragB@talentica-all.com>
2018-12-05 13:54:15 +0530

    Chore:Develop and test findtodobyid api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyparser = require('body-parser')
-
+const {ObjectID} = require('mongodb')
 const { mongoose } = require('./db/mongoose')
 const { Todo} = require('./models/todo')
 const {User} = require('./models/user')
@@ -24,6 +24,19 @@ app.get('/todos', (req, res) => {
   }, err => {
     res.status(400).send(err)
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+  Todo.findById(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+    res.status(200).send({ todo })
+  }, err => res.status(400).send(err))
 })
 
 app.listen(3000, () => {
