69e28c31778af40c59e6aacfb0208e44c8bc38d4
Anurag Bajaj <AnuragB@talentica-all.com>
2018-12-05 23:28:40 +0530

    Chore:Configuring app for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { Todo} = require('./models/todo')
 const {User} = require('./models/user')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use(bodyparser.json())
 
@@ -39,8 +40,8 @@ app.get('/todos/:id', (req, res) => {
   }, err => res.status(400).send(err))
 })
 
-app.listen(3000, () => {
-  console.log('Started on port 3000')
+app.listen(port, () => {
+  console.log(`Started on port ${port}`)
 })
 
 module.exports = {
